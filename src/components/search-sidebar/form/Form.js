5f4143668a8b9151ae7b67ee5e2e75b223b25dd8
Antoine Bourin <antoine.bourin.ext@eulerhermes.com>
2023-05-18 19:13:11 +0200

    feat: add sidebar form

--- src/components/search-sidebar/form/Form.js
+++ src/components/search-sidebar/form/Form.js
@@ -1,7 +1,51 @@
-import React from "react";
+"use client";
+import styles from "./Form.module.scss";
+import { useRouter, usePathname } from "next/navigation";
 
 const Form = () => {
-  return <div></div>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const searchParams = new URLSearchParams();
+    searchParams.append("sort_by", form.get("sort"));
+    searchParams.append("release_date.gte", form.get("fromDate"));
+    searchParams.append("release_date.lte", form.get("toDate"));
+
+    router.push(`${pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <h2>Filtrer</h2>
+      <div className={styles.date}>
+        <h3>Date de sortie</h3>
+        <div>
+          <p>Du</p>
+          <input type="date" name="fromDate" />
+        </div>
+        <div>
+          <p>au</p>
+          <input
+            type="date"
+            name="toDate"
+            defaultValue={new Date().toISOString().substring(0, 10)}
+          />
+        </div>
+      </div>
+      <div>
+        <h3>Trier par</h3>
+        <select name="sort">
+          <option value="popularity.desc">Popularité</option>
+          <option value="vote_average.desc">Note</option>
+          <option value="cote_count.desc">Nombre de notes</option>
+        </select>
+      </div>
+      <input type="submit" value="Rechercher" />
+    </form>
+  );
 };
 
 export default Form;
