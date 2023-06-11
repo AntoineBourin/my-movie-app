8c64610d6ddbf2b61a47bfb8b213d9a6a366f450
Antoine Bourin <antoine.bourin.ext@eulerhermes.com>
2023-06-11 14:52:32 +0200

    feat: add movie images and title

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Image from "next/image";
+import styles from "./MovieDetails.module.scss";
 
-const MovieDetails = () => {
-  return <div></div>;
+const MovieDetails = ({ movie }) => {
+  return (
+    <div className={styles.details}>
+      <div className={styles.background}>
+        <Image
+          src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_PATH}/original${movie.backdrop_path}`}
+          alt={movie.title}
+          fill
+        />
+      </div>
+      <div className={styles.content}>
+        <Image
+          src={`${process.env.NEXT_PUBLIC_TMDB_IMAGE_BASE_PATH}/w342${movie.poster_path}`}
+          width={250}
+          height={400}
+          alt={movie.title}
+        />
+        <div className={styles.description}>
+          <h1>
+            {movie.title}{" "}
+            <span className={styles.releaseDate}>
+              ({new Date(movie.release_date).toLocaleDateString("fr-FR")})
+            </span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetails;
